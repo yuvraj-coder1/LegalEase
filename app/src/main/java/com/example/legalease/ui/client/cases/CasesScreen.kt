b0388c0d99d444a5ec6651e7579f595e0538ff6f
Aman Kumar <amanksh05@gmail.com>
2024-05-25 11:30:05 +0530

    Chat screen modified

--- app/src/main/java/com/example/legalease/ui/client/cases/CasesScreen.kt
+++ app/src/main/java/com/example/legalease/ui/client/cases/CasesScreen.kt
@@ -9,12 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ui.theme.Inter
 
 @Composable
 fun CasesScreen(modifier: Modifier = Modifier) {
@@ -34,7 +35,9 @@ fun CasesScreen(modifier: Modifier = Modifier) {
                 description = it.description,
                 upcomingHearing = it.upcomingHearing,
                 onItemClick = {},
-                modifier = modifier.padding(16.dp).fillMaxWidth()
+                modifier = modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
             )
         }
     }
@@ -49,12 +52,23 @@ fun CaseItem(
     onItemClick: () -> Unit
 ) {
 
-    Card(modifier = modifier.clickable { onItemClick() }) {
+    Card(
+        modifier = modifier.clickable { onItemClick() },
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.onPrimary)
+    ) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = title, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+            Text(
+                text = title,
+                fontSize = 16.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colorScheme.primary
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = description,
+                fontFamily = Inter,
+                color = MaterialTheme.colorScheme.primaryContainer,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 3,
                 textAlign = TextAlign.Justify,
